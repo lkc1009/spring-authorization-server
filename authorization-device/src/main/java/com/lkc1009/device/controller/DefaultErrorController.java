621584d6436a61ba2179198ac269c5e4cc9da5c2
lkc1009 <lkc1009@users.noreply.github.com>
2023-08-22 15:24:45 +0800

    feat: 添加设备授权码测试页面

---
+++ authorization-device/src/main/java/com/lkc1009/device/controller/DefaultErrorController.java
@@ -0,0 +1,32 @@
+package com.lkc1009.device.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+	public String handleError(Model model, HttpServletRequest request) {
+		String errorMessage = getErrorMessage(request);
+
+		if (errorMessage.startsWith("[access_denied]")) {
+			model.addAttribute("errorTitle", "Access Denied");
+			model.addAttribute("errorMessage", "You have denied access.");
+		} else {
+			model.addAttribute("errorTitle", "Error");
+			model.addAttribute("errorMessage", errorMessage);
+		}
+		return "error";
+	}
+
+	private String getErrorMessage(@NotNull HttpServletRequest request) {
+		return (String) request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+	}
+
+}
---
+++ authorization-device/src/main/resources/device.sql
@@ -0,0 +1,9 @@
+INSERT INTO `oauth2_registered_client` (`id`, `client_id`, `client_id_issued_at`, `client_secret`,
+                                        `client_secret_expires_at`, `client_name`,
+                                        `client_authentication_methods`, `authorization_grant_types`,
+                                        `redirect_uris`, `post_logout_redirect_uris`, `scopes`,
+                                        `client_settings`, `token_settings`)
+VALUES ('d84e9e7c-abb1-46f7-bb0f-4511af362ca5', 'device-client-id', '2023-07-15 16:34:15', '', NULL, '智能设备', 'none',
+        'refresh_token,urn:ietf:params:oauth:grant-type:device_code', '', '', 'message.read,message.write',
+        '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":false}',
+        '\r\n{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",1800.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"self-contained\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",3600.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",1800.000000000],\"settings.token.device-code-time-to-live\":[\"java.time.Duration\",1800.000000000]}');
